dd91587c9b3a14beafab7c146fe72064574de7a1
Leonardo Cano <leocano4242@gmail.com>
2019-02-11 15:39:31 -0200

    Fix a route

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ const router = require('express').Router()
 const bookController = require('./controllers/bookController')
 
 router.post('/book', bookController.create)
-
-router.get('/book/:book_id', bookController.view)
+router.get('/books/:book_id', bookController.view)
 
 module.exports = router
