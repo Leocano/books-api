aaea0f25b0d3461d65f21847a99535aa6989bb16
Leonardo Cano <leocano4242@gmail.com>
2019-02-12 09:43:40 -0200

    Fetch ISBN from book links

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -28,31 +28,66 @@ exports.view = (req, res) => {
 exports.search = (req, res) => {
   const url = 'https://kotlinlang.org/docs/books.html'
   rp(url)
-    .then((html) => {
+    .then(html => {
       const $ = cheerio.load(html)
       const books = []
-      const titles = $('article.page-content h2').map((i, title) => {
+      const titles = $('article.page-content > h2').map((i, title) => {
         return $(title).text()
       }).get()
-      const languages = $('article.page-content .book-lang').map((i, language) => {
+
+      const languages = $('article.page-content > .book-lang').map((i, language) => {
         return $(language).text()
       }).get()
-      const descriptions = $('article.page-content p:not(p + p)').map((i, description) => {
+
+      const descriptions = $('article.page-content > p:not(p + p)').map((i, description) => {
         return $(description).text()
       }).get()
 
-      for(let i = 0;i < titles.length;i++) {
-        books.push({
-          title: titles[i],
-          language: languages[i],
-          description: descriptions[i]
+      const links = $('article.page-content > a').map((i, link) => {
+        return $(link).attr('href')
+      }).get()
+
+      const isbn = links.map(link => {
+        return new Promise((resolve, reject) => {
+          rp(link)
+            .then(html => {
+              const isbnPosition = html.indexOf("isbn")
+              if (isbnPosition >= 0) {
+                resolve(html.substring(isbnPosition + 6, isbnPosition + 19))
+              } else {
+                const isbnPosition = html.indexOf("ISBN")
+                if (isbnPosition >= 0) {
+                  resolve(html.substring(isbnPosition + 5, isbnPosition + 18))
+                } else {
+                  resolve("unavailable")
+                }
+              }
+            })
+            .catch(error => {
+              resolve("unavailable")
+            })
         })
-      }
-      
-      res.send({
-        numberBooks: books.length,
-        books: books
       })
+
+      Promise.all(isbn)
+        .then(isbn => {
+          for(let i = 0;i < titles.length;i++) {
+            books.push({
+              title: titles[i],
+              language: languages[i],
+              description: descriptions[i],
+              isbn: isbn[i]
+            })
+          }
+
+          res.send({
+            numberBooks: books.length,
+            books: books
+          })
+        })
+        .catch(error => {
+          res.send(error)
+        })
     })
     .catch((err) => {
       res.send(err)
